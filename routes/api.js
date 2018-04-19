90c66d86df1ccf6a0886e05ea42c8d43501949a6
Andriy <AndriyKuf@gmail.com>
2018-04-19 13:37:42 +0200

    *switched api to mieszkobuliktrainingsystem.pl

--- routes/api.js
+++ routes/api.js
@@ -73,6 +73,32 @@ router.get('/user', function(req, res, next) {
 });
 
 
+// send Mail block
+const transporter = require('../nodemailer/transporter');
+
+/* GET home page. */
+router.post('/order', function(req, res) {
+    transporter.sendMail(getMailOptions(req.body || {}), (err, result) => {
+        if (err) {
+            console.log(err);
+            res.end(err);
+        } else {
+            console.log(req.body);
+            res.end('good');
+        }
+    })
+});
+
+function getMailOptions(dto = {}) {
+    return {
+        from: 'AndriyKuf@gmail.com',
+        to: 'dronozzavr@gmail.com',
+        subject: 'Nodemailer test',
+        text: `Name: ${dto.name || 'empty'}; E-mail: ${dto.mail || 'empty'}; Phone: ${dto.phone || 'empty'}; Message: ${dto.msg || 'empty'}`
+    }
+}
+
+
 function forbidden(req, res, next) {
     console.log(req.user);
     if (req.user && req.user.rights === 'mieszko') {
